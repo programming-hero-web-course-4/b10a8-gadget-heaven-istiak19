24eb5717588943687967eae4681ee32f3f367525
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-05 09:45:08 +0600

    tenth commit

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -9,12 +9,14 @@ const MainLayout = () => {
     const [cart, setCart] = useState([])
     return (
         <div>
-            <CartContext.Provider value={[cart,setCart]}>
+            <CartContext.Provider value={[cart, setCart]}>
                 <NavBar></NavBar>
-                <div className="min-h-[calc(100vh-288px)]">
+                <div className="min-h-[calc(100vh-88px)]">
                     <Outlet></Outlet>
                 </div>
-                <Footer></Footer>
+                <div className="mt-48">
+                    <Footer></Footer>
+                </div>
             </CartContext.Provider>
         </div>
     );
--- src/Components/ProductDetails/ProductDetails.jsx
+++ src/Components/ProductDetails/ProductDetails.jsx
@@ -3,7 +3,7 @@ import { useLoaderData, useParams } from "react-router-dom";
 import { CartContext } from "../MainLayout/MainLayout";
 
 const ProductDetails = () => {
-    const [cart,setCart]=useContext(CartContext)
+    const [cart, setCart] = useContext(CartContext)
     const { id } = useParams()
     const idIn = parseInt(id)
     const data = useLoaderData()
@@ -12,36 +12,49 @@ const ProductDetails = () => {
         const findProduct = data.find(product => product.product_id === idIn)
         setProduct(findProduct)
     }, [data, idIn])
+
     const { rating, availability, specification, description, price, category, product_image, product_id, product_title } = product
 
 
     return (
-        <div className="hero my-32">
-            <div className="hero-content flex-col lg:flex-row">
-                <img
-                    src={product_image}
-                    className="w-[424.31px] h-[503px] rounded-lg" />
-                <div className="space-y-3">
-                    <h1 className="text-2xl font-bold">{product_title}</h1>
-                    <p className="font-semibold">Price: {price} $</p>
-                    <button className="text-xs px-3 py-2 rounded-full bg-green-200 text-green-600 border border-green-600">{availability ? 'In Stock' : 'Out Of Stock'}</button>
-                    <p className="text-gray-500">{description}</p>
-                    <p className="font-bold">Rating ⭐ </p>
-                    <div className="flex items-center text-xs">
-                        <div className="rating mr-3">
-                            <input type="radio" name="rating-1" className="mask mask-star" />
-                            <input type="radio" name="rating-1" className="mask mask-star" />
-                            <input type="radio" name="rating-1" className="mask mask-star" />
-                            <input type="radio" name="rating-1" className="mask mask-star" defaultChecked />
-                            <input type="radio" name="rating-1" className="mask mask-star" />
+        <div className="relative">
+            <div className="bg-[#9538E2] text-center space-y-3 pt-8 pb-60">
+                <h2 className="text-3xl text-white font-bold">Product Details</h2>
+                <p className="text-white">Explore the latest gadgets that will take your experience to the next level. From smart devices to <br /> the coolest accessories, we have it all!</p>
+            </div>
+            <div className="absolute w-[800px] shadow-xl bg-slate-100 rounded-lg p-5 border border-pink-500 top-40 left-72">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img
+                        src={product_image}
+                        className="w-[300px] h-[350px] rounded-lg" />
+                    <div className="space-y-3">
+                        <h1 className="text-xl font-bold">{product_title}</h1>
+                        <p className="font-semibold">Price: {price} $</p>
+                        <button className="text-xs px-3 py-2 rounded-full bg-green-200 text-green-600 border border-green-600">{availability ? 'In Stock' : 'Out Of Stock'}</button>
+                        <p className="text-gray-500 font-light">{description}</p>
+                        <p className="font-bold">Rating ⭐ </p>
+                        <div>
+                            <ul className="font-light text-gray-500">
+                                {
+                                    specification && specification.map((s, idx) => <li key={idx}>{idx + 1}. {s}</li>)
+                                }
+                            </ul>
                         </div>
-                        <p className="bg-gray-300 rounded-full px-1 py-2">{rating}</p>
+                        <div className="flex items-center text-xs">
+                            <div className="rating mr-3">
+                                <input type="radio" name="rating-1" className="mask mask-star" />
+                                <input type="radio" name="rating-1" className="mask mask-star" />
+                                <input type="radio" name="rating-1" className="mask mask-star" />
+                                <input type="radio" name="rating-1" className="mask mask-star" defaultChecked />
+                                <input type="radio" name="rating-1" className="mask mask-star" />
+                            </div>
+                            <p className="bg-gray-300 rounded-full px-1 py-2">{rating}</p>
+                        </div>
+                        <button onClick={() => setCart(product_id)} className="btn bg-[#9538E2] text-white rounded-full mr-5">Add To Card <i className="fa-solid fa-cart-shopping"></i></button>
+                        <button className="btn border border-black rounded-full">
+                            <i className="fa-solid fa-heart"></i>
+                        </button>
                     </div>
-                    <button onClick={()=>setCart(product_id)} className="btn bg-[#9538E2] text-white rounded-full mr-5">Add To Card <i className="fa-solid fa-cart-shopping"></i></button>
-                    <button className="btn border border-black rounded-full">
-                        <i className="fa-solid fa-heart"></i>
-                    </button>
-
                 </div>
             </div>
         </div>
