53da20aa0687b1c827c15096c1fabf7c53b47ade
istiak19 <istiak15-14128@diu.edu.bd>
2024-11-05 12:36:05 +0600

    twelfth commit

--- src/Components/WishList/WishList.jsx
+++ src/Components/WishList/WishList.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+import { getStoredWishList } from "../utility/Utility";
 const WishList = () => {
+    const [products, setProducts] = useState([]);
+    const [wishlist, setWishList] = useState([])
+    useEffect(() => {
+        fetch('../products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+
+    useEffect(() => {
+        const storedWishList = getStoredWishList()
+        const storedWishListIn = storedWishList.map(id => parseInt(id))
+        const wishListFilter = products.filter(product => storedWishListIn.includes(product.product_id))
+        setWishList(wishListFilter)
+    }, [products])
     return (
         <div>
-            <h2>wishlist</h2>
+            <h2>wishlist {wishlist.length}</h2>
         </div>
     );
 };
